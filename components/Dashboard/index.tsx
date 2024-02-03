1ae4fce9730c4d527e93088b5e7260c6ef30c207
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-02-03 17:26:22 +0000

    marquee?

--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,7 +1,11 @@
+import Script from "next/script";
+
 export default function Dashboard () {
     return (
         <div>
-            
+            <div className="livecoinwatch-widget-5 manrope glow-blue" lcw-base="USD" lcw-color-tx="#999999" lcw-marquee-1="coins" lcw-marquee-2="movers" lcw-marquee-items="10" ></div>
+
+            <Script defer src="https://www.livecoinwatch.com/static/lcw-widget.js" strategy="beforeInteractive" onLoad={() => alert("loaded")}></Script>
         </div>
     );
 }
